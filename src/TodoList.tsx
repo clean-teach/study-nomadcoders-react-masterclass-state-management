6e5f3d87efec19740b56bc98f8a03235b6f4884a
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-14 03:06:11 +0900

    #6.6 Forms
    ## React-hook-form
    
    [Home](https://react-hook-form.com/)
    
    - react에서 form 작업을 구현 하는 가장 편리한 도구
    
    ### 배경
    
    1. react에서 form 작업을 구현하기 위해서 각 input 마다, state, event listener, error state, 유효성 검사 등이 필요하다.
    2. 어떠한 경우에는 form 하나에도 수많은 input이 존재하며, 존재하는 input 수만큼 매번 위와같은 작업을 반복하여야 한다.
    
    → **React-hook-form** 은 이런 작업을 직접 하지 않도록 도와준다.
    
    → 생산성에 큰 도움이 된다.
    
    ### 사용법
    
    1. `npm install react-hook-form` 설치
    2. form component에서 사용
    
        ```tsx
        **import { useForm } from "react-hook-form";**
    
        **const { register, watch } = useForm();**
    
        console.log(**watch()**);
    
        return (
          <form>
        		<input **{...register("name")}** />
        ```
    
        1. `import { useForm } from "react-hook-form";` 불러오기
        2. **`const { register, watch } = useForm();` : `useForm()`** 함수를 사용해서 **`register, watch`** 등 함수 불러오기
            - **`register()`** :
                - input에서 사용하는 다양한 event handler 등이 들어 있다.
                - 각 input 마다 구분 할 name값을 props로 넘겨준다.
            - **`watch()`** : 관리되고 있는 input 값에 접근 할 수 있다.
        3. `<input />` 에 [Spread 연산자](https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Spread_syntax) 를 사용해서 **`{...register("name")}`** 를 props로 보내준다.

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,27 +1,73 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// function TodoList() {
+//   const [toDo, setToDo] = useState('');
+//   const [toDoError, setToDoError] = useState('');
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDo(value);
+//     setToDoError('');
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (toDo.length < 10) {
+//       return setToDoError('To do should be longer');
+//     }
+//     console.log('submit');
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           type="text"
+//           placeholder="Write a to do"
+//           value={toDo}
+//           onChange={onChange}
+//         />
+//         <button>Add</button>
+//         {toDoError !== '' ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// }
 
 function TodoList() {
-  const [toDo, setToDo] = useState('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
-
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
+      <form>
+        <input type="text" placeholder="Write a to do" {...register('email')} />
         <input
           type="text"
           placeholder="Write a to do"
-          value={toDo}
-          onChange={onChange}
+          {...register('firstName')}
         />
+        <input
+          type="text"
+          placeholder="Write a to do"
+          {...register('lastName')}
+        />
+        <input
+          type="text"
+          placeholder="Write a to do"
+          {...register('password')}
+        />
+        <input
+          type="text"
+          placeholder="Write a to do"
+          {...register('password2')}
+        />
+        <input
+          type="text"
+          placeholder="Write a to do"
+          {...register('userName')}
+        />
+        <button>Add</button>
       </form>
     </div>
   );
