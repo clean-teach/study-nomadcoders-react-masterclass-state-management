b7b8cf51676cfda4e29f25650bb9be6897d28571
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-14 20:10:24 +0900

    #6.10 Recap
    - `React-hook-form` 에 대해서 배운 내용을 바탕으로 우리의 프로젝트에 적용해보며 복습한다.
    
    ## `[setValue()](https://react-hook-form.com/api/useform/setvalue)`
    
    `setValue('inputName', 'inputValue')`
    
    - 특정 input의 값을 임의로 입력 할 수 있다.
    - 첫 번째 인수 : input 의 이름
    - 두 번째 인수 : 바꿀 값

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,131 +1,25 @@
 import { useForm } from 'react-hook-form';
 
-// function TodoList() {
-//   const [toDo, setToDo] = useState('');
-//   const [toDoError, setToDoError] = useState('');
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDo(value);
-//     setToDoError('');
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     if (toDo.length < 10) {
-//       return setToDoError('To do should be longer');
-//     }
-//     console.log('submit');
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Write a to do"
-//           value={toDo}
-//           onChange={onChange}
-//         />
-//         <button>Add</button>
-//         {toDoError !== '' ? toDoError : null}
-//       </form>
-//     </div>
-//   );
-// }
-
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  password: string;
-  password1: string;
-  username: string;
-  extraError: string;
+  toDo: string;
 }
 
 function TodoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({
-    defaultValues: {
-      email: '@naver.com',
-    },
-  });
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        'password1',
-        { message: 'Password are not the same' },
-        { shouldFocus: true },
-      );
-    }
-    setError('extraError', { message: 'Server offline.' });
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log('add to do : ', data);
+    setValue('toDo', '');
   };
-
-  console.log(errors);
 
   return (
     <div>
-      <form
-        style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={handleSubmit(onValid)}
-      >
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register('email', {
-            required: 'Email is required',
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: 'Only naver.com emails allowed',
-            },
-          })}
-          placeholder="Email"
+          type="text"
+          placeholder="Write a to do"
+          {...register('toDo', { required: 'Please write a To Do' })}
         />
-
-        <input
-          {...register('firstName', {
-            required: 'write here',
-            validate: {
-              noNico: (value) =>
-                value.includes('nico') ? 'no nico allowed' : true,
-              noMong: (value) =>
-                value.includes('mong') ? 'no mong allowed' : true,
-            },
-          })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register('lastName', { required: 'write here' })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register('username', { required: 'write here', minLength: 10 })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register('password', { required: true, minLength: 5 })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register('password1', {
-            required: 'Password is required',
-            minLength: {
-              value: 5,
-              message: 'Your password is too short.',
-            },
-          })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
