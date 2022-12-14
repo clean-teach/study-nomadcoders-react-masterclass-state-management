3a75561c94cb816634e393d7bdaad285eb60d922
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-15 02:34:58 +0900

    #6.18 Enums
    ## 목표
    
    - 현재 열려있는 카테고리 상태에 따라서 등록하는 할 일 목록의 카테고리도 결정 되게 구현해 본다.
    
    ## 구현
    
    1. 할 일 목록을 등록 할때 현재 열려있는 카테고리를 알아와야 하므로 `src/components/CreateToDo.tsx` 컴포넌트에서 작업한다.
    
        ```tsx
        function CreateToDo() {
          const **category** = useRecoilValue(categoryState);
        	...
            setToDos((oldTodos) => [
              { text: toDo, id: Date.now(), **category** },
              ...oldTodos,
            ]);
        ```
    
        1. 현재 category State를 조회하여, 추가 될 할 일 목록의 category 값으로 입력한다.
    2. [현재 `ToDo` atom의 `category` 항목은 `'TO_DO' | 'DOING' | 'DONE'` 선택지로 Type이 고정되어 있](https://www.notion.so/06-STATE-MANAGEMENT-5e1149e6812643cd9a00f3de10434f59)기 때문에 카테고리로 입력받는 String 과 달라 Type Error가 발생한다.
    
        → `[type](https://www.typescriptlang.org/ko/docs/handbook/utility-types.html#partialtype)` keyword 를 사용해서 반복되는 type을 재활용 할 수 있다.
    
        ```tsx
        type categories = 'TO_DO' | 'DOING' | 'DONE';
    
        export interface IToDo {
        ...
          category: categories;
        ...
        export const categoryState = atom<categories>({
        ...
        ```
    
        - 하지만, 프로젝트 전역에서 사용할 수 있는 좀 더 범용성 있는 방법은 `enums`이다.
        - `type` 은 그냥 복붙을 안하게 해주는 단순한 문법이다.
    
    ### `[enums](https://www.typescriptlang.org/ko/docs/handbook/enums.html)`
    
    - enumerable
    - **열거형**
    - 원하는 문자열로 열거형을 만들면 프로젝트 어디서든 동일한 Type의 선택지를 사용 할 수 있다.
        - `enum` 안에 문자열 한번만 적고 이를 사용하므로, 코드를 보호 받을 수 있다.
    - `eunm` 은 따로 값을 명시 하지 않으면 기본적으로 숫자형으로 변환된다.
    
        ```tsx
        export enum Categories {
          'TO_DO', // = 0
          'DOING', // = 1
          'DONE', // = 2
        }
        ```
    
        - 개발자 입장에서 문자열로 기억하는 것이지, 컴퓨터 입장에서는 숫자로 분류하는 것이다.
    - 숫자형이 아닌, 명확한 값을 원한다면 문자형으로 명시해 줄 수도 있다.
    
        ```tsx
        export enum Categories {
          'TO_DO' = 'TO_DO',
          'DOING' = 'DOING',
          'DONE' = 'DONE',
        }
        ```
    
    - `export` 하면 프로젝트 어디서든 객체 형식과 비슷하게 불러와서 사용 할 수 있다.
    
    ## 참고
    
    - [ ]  왜 `enum`을 사용 함에도 *`as* any` 를 사용하지 않으면 Type Error 가 발생하는 것인가?

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,17 +1,18 @@
 import { useForm } from 'react-hook-form';
-import { useSetRecoilState } from 'recoil';
-import { toDoState } from '../atoms';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { categoryState, toDoState } from '../atoms';
 
 interface IForm {
   toDo: string;
 }
 
 function CreateToDo() {
+  const category = useRecoilValue(categoryState);
   const setToDos = useSetRecoilState(toDoState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
   const handleValid = ({ toDo }: IForm) => {
     setToDos((oldTodos) => [
-      { text: toDo, id: Date.now(), category: 'TO_DO' },
+      { text: toDo, id: Date.now(), category },
       ...oldTodos,
     ]);
     setValue('toDo', '');
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { categoryState, toDoSelector } from '../atoms';
+import { Categories, categoryState, toDoSelector } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
@@ -9,7 +9,7 @@ function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
   const onSelect = (event: React.FormEvent<HTMLSelectElement>) => {
     // console.log(event.currentTarget.value);
-    setCategory(event.currentTarget.value);
+    setCategory(event.currentTarget.value as any);
   };
 
   return (
@@ -17,9 +17,9 @@ function ToDoList() {
       <h1>To Dos</h1>
       <hr />
       <select value={category} onInput={onSelect}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <hr />
       <CreateToDo />
