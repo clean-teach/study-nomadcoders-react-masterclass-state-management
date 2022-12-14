6bb10379d7248f17079a5156d976d15fb296242e
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-14 22:31:07 +0900

    #6.12 Refactoring
    ## 목표
    
    - 현재 하나의 파일에 구현되어 있는 코드는 Component 별로 분류하여 파일을 만든다.
    
        > *코드가 조금 더러워도 일단 작동하게 구현하고, 그 후에 코드를 정리하는 방식이 무작정 모든 파일을 만드는 것보다 나은 방법이다.*
        >
    
    ## 폴더구조
    
    - `src`
        - `index.tsx`
        - `App.tsx`
        - `theme.ts`
        - components
            - `ToDoList.tsx`
            - `ToDo.tsx`
            - `CreateToDo.tsx`
        - `atoms.tsx`
    
    → Recoil을 이용하면 atom으로만 연결되기 때문에, 서로 props로 종속되지 않은 Component로 분류 할 수 있다.
    
    ## Props 를 전달하는 편리한 방법
    
    ```tsx
    {toDos.map((toDo) => (
      <ToDo key={toDo.id} {...toDo} />
    ))}
    ```
    
    - 전개 구문을 이용하면 짧은 코드만으로 많은 Props 를 보낼 수 있다.
        - 단, 보내는 Props의 형태와 받는 쪽의 Type 형태가 같아야 한다.
            - 같은 `interface`를 공유한다면, 형태가 같기 때문에 위와 같은 문법이 가능하다.

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,48 +1,19 @@
-import { useForm } from 'react-hook-form';
-import { atom, useRecoilState } from 'recoil';
-
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {
-  text: string;
-  id: number;
-  category: 'TO_DO' | 'DOING' | 'DONE';
-}
-
-const toDoState = atom<IToDo[]>({
-  key: 'toDo',
-  default: [],
-});
+import { useRecoilValue } from 'recoil';
+import { toDoState } from '../atoms';
+import CreateToDo from './CreateToDo';
+import ToDo from './ToDo';
 
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = ({ toDo }: IForm) => {
-    setToDos((oldTodos) => [
-      { text: toDo, id: Date.now(), category: 'TO_DO' },
-      ...oldTodos,
-    ]);
-    setValue('toDo', '');
-    console.log(toDos);
-  };
+  const toDos = useRecoilValue(toDoState);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          type="text"
-          placeholder="Write a to do"
-          {...register('toDo', { required: 'Please write a To Do' })}
-        />
-        <button>Add</button>
-      </form>
+      <CreateToDo />
       <ul>
         {toDos.map((toDo) => (
-          <li key={toDo.id}>{toDo.text}</li>
+          <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
     </div>
